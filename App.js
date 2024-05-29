2f761ce36b4237cb2a6f538dc39e998caf3e9177
Alan Jian <jian@ischool.berkeley.edu>
2024-05-29 01:08:55 -0700

    rough sketch of board object

--- App.js
+++ App.js
@@ -1,27 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, SafeAreaView } from 'react-native';
 
 const PlaceholderImage = require('./assets/background-image.png');
 
 const Tile = ({letter}) => {
   return (
     <View>
-      <Pressable style={{ backgroundColor: '#147EFB', padding: 10, borderRadius: 4 }}>
-        <Text> {letter} </Text>
+      <Pressable style={styles.pressable}>
+        <Text style={styles.pressableText}> {letter} </Text>
       </Pressable>
     </View>
   );
 };
 
-const Map = () => {
-
+const Board = ({letters}) => {
+  const listLetters = letters.map(letter => <Tile letter={letter} />)
+  return (
+    <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>
+      {listLetters}
+    </View>
+  )
 }
 
 export default function App() {
+  const letters = "ABCDEFG".split('')
   return (
-    <View style={styles.container}>
-      <Tile letter="X" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Board letters={letters}/>
+    </SafeAreaView>
   );
 }
 
@@ -32,4 +38,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  pressable: {
+    backgroundColor: '#147EFB', 
+    padding: 10, 
+    borderRadius: 4
+  },
+  pressableText: {
+
+  }
 });
